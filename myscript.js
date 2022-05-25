b20de6ba825ed9377d7dfe4d4a46954e403080dd
Zahra-Abdolalizade <zabdolalizade149@gmail.com>
2022-05-25 13:07:30 +0430

    first commit

---
+++ myscript.js
@@ -0,0 +1,12 @@
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    const element = document.querySelector('.submit');
+    element.style.backgroundColor = 'white';
+    element.style.color =' hsl(25, 97%, 53%)';
+});
+
+const button1 = document.querySelector('button1');
+button1.addEventListener('click', () => {
+    const element = document.querySelector('.circle');
+    element.style.backgroundColor = ' hsl(25, 97%, 53%)'; 
+});
